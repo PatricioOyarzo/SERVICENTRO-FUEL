9c28a65595a45f44c7651e295b917fd184973db0
Pato <patricio.oyarzo@usm.cl>
2023-07-07 04:26:14 -0400

    agregue las apps a core y cree urls

--- FUEL/FUEL/urls.py
+++ FUEL/FUEL/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
+    path('api/', include('rest.urls')),
+    path('' , views.home, name='home'),
+    path('info/', views.Tgasolina,name='info'),
+    
+
 ]
